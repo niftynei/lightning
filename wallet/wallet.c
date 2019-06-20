01b1d7f2bb71af566bf78d8a50e8857013ad1e27
lisa neigut <niftynei@gmail.com>
2019-06-20 12:59:15 -0500

    fixup: wallet_compute_max - make compilieable

--- wallet/wallet.c
+++ wallet/wallet.c
@@ -476,7 +476,7 @@ const struct utxo **wallet_select_all(const tal_t *ctx, struct wallet *w,
 	return utxo;
 }
 
-const void wallet_compute_max(const tal_t ctx, struct wallet *w,
+const void wallet_compute_max(const tal_t *ctx, struct wallet *w,
 			      u32 max_utxos, struct amount_sat *sat)
 {
 	size_t i = 0;
