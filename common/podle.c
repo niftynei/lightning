ff6df61a4b8ec2133a5bfd2235dd2ff32b5184de
lisa neigut <niftynei@gmail.com>
2020-02-11 15:10:37 -0600

    podle: Port JoinMarket's PoDLE
    Re-implementation of the Proof of Discrete Log Equivalence that
    JoinMarket uses.
    
    As per the most recent mailing list discussion, this will be updated
    shortly for a Lightning-specific attack mitigation
    
    See also https://joinmarket.me/blog/blog/poodle/

--- common/podle.c
+++ common/podle.c
@@ -1,10 +1,63 @@
+#include <assert.h>
 #include <bitcoin/privkey.h>
 #include <bitcoin/pubkey.h>
 #include <ccan/array_size/array_size.h>
-#include <ccan/crypto/sha256/sha256.h>
 #include <common/podle.h>
 #include <common/utils.h>
+#include <secp256k1.h>
+#include <sodium/randombytes.h>
 #include <stdio.h>
+
+/* e = [ kG || kJ || P || P2 ], all of which
+ * are of size PUBKEY_CMPR_LEN */
+#define E_MEMBERS 4
+#define E_LEN E_MEMBERS * PUBKEY_CMPR_LEN
+
+static bool init_G(struct pubkey *G)
+{
+	/* G's seed is the scalar value 'one' */
+	unsigned char g_priv[32] = {0};
+	g_priv[31]++;
+
+	/* Convert the scalar 'one' into a point value, G */
+	if (!secp256k1_ec_pubkey_create(secp256k1_ctx, &G->pubkey, g_priv))
+		return false;
+
+	return true;
+}
+
+#ifdef SUPERVERBOSE
+static void print_arr(unsigned char *arr, size_t len)
+{
+	size_t j;
+	for (j = 0; j < len; j++)
+		fprintf(stderr, "%02x", ((u8 *)arr)[j]);
+	fprintf(stderr, "\n");
+}
+#endif
+
+/* Assumes that pubkeys are packed in correct order (kG, kJ, P, P2) */
+static bool pack_e(unsigned char *e_arr, size_t e_len, struct pubkey **pubkeys, size_t pk_len)
+{
+	unsigned char compressed[PUBKEY_CMPR_LEN];
+	size_t clen = sizeof(compressed), i;
+
+	assert(e_len == pk_len * PUBKEY_CMPR_LEN);
+
+	for (i = 0; i < pk_len; i++) {
+		if (!secp256k1_ec_pubkey_serialize(secp256k1_ctx, compressed,
+						   &clen, &pubkeys[i]->pubkey,
+						   SECP256K1_EC_COMPRESSED))
+			return false;
+
+#ifdef SUPERVERBOSE
+		print_arr(compressed, clen);
+#endif
+		memcpy(e_arr + PUBKEY_CMPR_LEN * i, compressed, clen);
+	}
+
+	return true;
+}
 
 static bool find_NUM(unsigned char *arr, size_t arr_len, struct pubkey *nums_key)
 {
@@ -13,7 +66,10 @@ static bool find_NUM(unsigned char *arr, size_t arr_len, struct pubkey *nums_key
 
 	/* Placeholder for sha-result + compression marker */
 	unsigned char cmp_key[PUBKEY_CMPR_LEN];
-	size_t len = ARRAY_SIZE(cmp_key);
+	size_t len = sizeof(cmp_key);
+
+	/* Set the first bit to 0x02, denoting positive compressed
+	 * pubkey serialization */
 	((u8 *)cmp_key)[0] = 2;
 
 	for (i = 0; i < 256; i++) {
@@ -36,14 +92,9 @@ bool derive_NUMS_point_at(u8 index, struct pubkey *nums_key)
 	/* G (compressed/uncompressed) + 1-byte index + 1-byte counter */
 	unsigned char seed_cmp_g[PUBKEY_CMPR_LEN + 2];
 	unsigned char seed_uncmp_g[65 + 2];
-	size_t c_len = ARRAY_SIZE(seed_cmp_g), uc_len = ARRAY_SIZE(seed_uncmp_g), outlen;
+	size_t c_len = sizeof(seed_cmp_g), uc_len = sizeof(seed_uncmp_g), outlen;
 
-	/* G seed (the first!) */
-	unsigned char g_priv[32] = {0};
-	g_priv[31]++;
-
-	/* First we need to get the G point */
-	if (!secp256k1_ec_pubkey_create(secp256k1_ctx, &G.pubkey, g_priv))
+	if (!init_G(&G))
 		return false;
 
 	/* Pull out the compressed serializiation of G */
@@ -64,4 +115,167 @@ bool derive_NUMS_point_at(u8 index, struct pubkey *nums_key)
 		return false;
 	seed_uncmp_g[uc_len - 2] = index;
 	return find_NUM(seed_cmp_g, uc_len, nums_key);
+}
+
+bool generate_proof_of_dle(struct privkey *x, struct pubkey *j,
+			   struct proof_dle *podle)
+{
+	/* A proof of discrete log equivalence requires:
+	 *  - a private key, x
+	 *  - a second generator point, j
+	 *  - a nonce, k (generated herein)
+	 *
+	 *  With these, we calculate
+	 *  - the point k*G and k*J
+	 *  - the point 'P2', x*J
+	 *  - the point 'P', x*G
+	 *  - the commitment, sha256(P2)
+	 *  - e, sha256(kG || kJ || P || P2 ), and finally
+	 *  - the signature, k + x * e
+	 */
+	struct pubkey kG, p, kJ, p2, * pubkeys[E_MEMBERS];
+	unsigned char compressed[PUBKEY_CMPR_LEN];
+	unsigned char e_arr[E_LEN];
+	size_t clen = sizeof(compressed), pk_len = ARRAY_SIZE(pubkeys),
+	       elen = sizeof(e_arr);
+
+	/* Init our nonce `k` */
+	u8 k[32];
+	randombytes_buf(k, sizeof(k));
+
+	kJ = *j;
+	p2 = *j;
+
+	/* First we find kG, kJ, p + p2 */
+	if (!secp256k1_ec_pubkey_create(secp256k1_ctx, &kG.pubkey, k))
+		return false;
+
+	if (!secp256k1_ec_pubkey_tweak_mul(secp256k1_ctx, &kJ.pubkey, k))
+		return false;
+
+	if (!secp256k1_ec_pubkey_create(secp256k1_ctx, &p.pubkey, x->secret.data))
+		return false;
+
+	if (!secp256k1_ec_pubkey_tweak_mul(secp256k1_ctx, &p2.pubkey, x->secret.data))
+		return false;
+
+	/* Serialize the pubkeys (compressed) and pack into e's preimage */
+	pubkeys[0] = &kG;
+	pubkeys[1] = &kJ;
+	pubkeys[2] = &p;
+	pubkeys[3] = &p2;
+	if (!pack_e(e_arr, elen, pubkeys, pk_len))
+		return false;
+
+	/* Compute the commitment */
+	if (!secp256k1_ec_pubkey_serialize(secp256k1_ctx, compressed,
+					   &clen, &p2.pubkey,
+					   SECP256K1_EC_COMPRESSED))
+		return false;
+
+	sha256(&podle->commitment, compressed, clen);
+
+	/* Compute e */
+	sha256(&podle->e, e_arr, elen);
+	memcpy(&podle->sig, podle->e.u.u8, sizeof(podle->e.u.u8));
+
+	/* Compute the signature */
+	if (!secp256k1_ec_privkey_tweak_mul(secp256k1_ctx, podle->sig, x->secret.data)) {
+		// Is it weird to release the object here? I don't want to
+		// 'return' a half-filled in object
+		podle = tal_free(podle);
+		return false;
+	}
+
+	if (!secp256k1_ec_privkey_tweak_add(secp256k1_ctx, podle->sig, k)) {
+		podle = tal_free(podle);
+		return false;
+	}
+
+	/* Fill in p2 for the result */
+	podle->pk2 = p2;
+
+	return true;
+}
+
+bool verify_proof_of_dle(struct pubkey *p, struct pubkey *j, struct proof_dle *podle)
+{
+	/*
+	 * From https://joinmarket.me/blog/blog/poodle/
+	 *
+	 * Given P, P2, s, e and the original commitment, verify:
+	 *  - sha256(P2) is equal to the original commitment
+	 *  - kG = sG - eP
+	 *  - kJ = sJ - eP2
+	 *  - sha256(kG || kJ || P || P2) == e
+	 */
+	struct sha256 sha;
+	const secp256k1_pubkey *args[2];
+	struct pubkey eP, eP2, sG, sJ, kG, kJ, * pubkeys[E_MEMBERS];
+	unsigned char compressed[PUBKEY_CMPR_LEN], neg_e[32],
+		      e_arr[E_LEN];
+	size_t clen = sizeof(compressed), elen = sizeof(e_arr),
+	       pk_len = ARRAY_SIZE(pubkeys);
+
+	sJ = *j;
+	eP = *p;
+	eP2 = podle->pk2;
+
+	/* First, we verify that the sha256 of p2 is equivalent */
+	if (!secp256k1_ec_pubkey_serialize(secp256k1_ctx, compressed,
+					   &clen, &podle->pk2.pubkey,
+					   SECP256K1_EC_COMPRESSED))
+		return false;
+
+	sha256(&sha, compressed, clen);
+	if (!sha256_eq(&sha, &podle->commitment))
+		return false;
+
+	if (!init_G(&sG))
+		return false;
+
+	/* -e */
+	memcpy(neg_e, podle->e.u.u8, sizeof(podle->e.u.u8));
+	if (!secp256k1_ec_privkey_negate(secp256k1_ctx, neg_e))
+		return false;
+
+	/* sG */
+	if (!secp256k1_ec_pubkey_tweak_mul(secp256k1_ctx, &sG.pubkey, podle->sig))
+		return false;
+
+	/* -eP */
+	if (!secp256k1_ec_pubkey_tweak_mul(secp256k1_ctx, &eP.pubkey, neg_e))
+		return false;
+
+	/* kG = sG - eP */
+	args[0] = &sG.pubkey;
+	args[1] = &eP.pubkey;
+	if (!secp256k1_ec_pubkey_combine(secp256k1_ctx, &kG.pubkey, args, 2))
+		return false;
+
+	/* sJ */
+	if (!secp256k1_ec_pubkey_tweak_mul(secp256k1_ctx, &sJ.pubkey, podle->sig))
+		return false;
+
+	/* -eP2 */
+	if (!secp256k1_ec_pubkey_tweak_mul(secp256k1_ctx, &eP2.pubkey, neg_e))
+		return false;
+
+	/* kJ = sJ - eP2 */
+	args[0] = &sJ.pubkey;
+	args[1] = &eP2.pubkey;
+	if (!secp256k1_ec_pubkey_combine(secp256k1_ctx, &kJ.pubkey, args, 2))
+		return false;
+
+	/* Find 'e' */
+	pubkeys[0] = &kG;
+	pubkeys[1] = &kJ;
+	pubkeys[2] = p;
+	pubkeys[3] = &podle->pk2;
+	if (!pack_e(e_arr, elen, pubkeys, pk_len))
+		return false;
+	sha256(&sha, e_arr, elen);
+
+	/* Return comparison result */
+	return sha256_eq(&sha, &podle->e);
 }
--- common/podle.h
+++ common/podle.h
@@ -1,8 +1,16 @@
 #ifndef LIGHTNING_COMMON_PODLE_H
 #define LIGHTNING_COMMON_PODLE_H
 #include "config.h"
+#include <ccan/crypto/sha256/sha256.h>
 #include <ccan/short_types/short_types.h>
 #include <stdbool.h>
+
+struct proof_dle {
+	struct pubkey pk2;
+	struct sha256 commitment;
+	u8 sig[32];
+	struct sha256 e;
+};
 
 /* derive_NUMS_point_at - Find the starting NUMS point for
  * a Proof of Discrete Log Equivalence, given an index.
@@ -11,4 +19,24 @@
  */
 bool derive_NUMS_point_at(u8 index, struct pubkey *nums_key);
 
+/* generate_proof_of_dle - Given a private key and a
+ * secondary generator point, j, compute the proof of discrete
+ * log equivalence for this private key
+ *
+ * @privkey - (in) the private key to calculate the podle for
+ * @j       - (in) the second generator point
+ * @podle   - (out) commitment, etc
+ */
+bool generate_proof_of_dle(struct privkey *privkey, struct pubkey *j,
+			   struct proof_dle *podle);
+
+/* verify_proof_of_dle - Given the pubkey for the utxo, the generator
+ * point J, and the podle information, verify that it's a correct
+ * proof.
+ *
+ * @p     - (in) the pubkey of the utxo
+ * @j     - (in) the second generator point used for calculating the proof
+ * @podle - (in) the proof of discrete log equivalence info
+ */
+bool verify_proof_of_dle(struct pubkey *p, struct pubkey *j, struct proof_dle *podle);
 #endif /* LIGHTNING_COMMON_PODLE_H */
