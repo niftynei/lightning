1b5195953f67e344f50953ceae7888d86e195872
niftynei <niftynei@gmail.com>
2020-09-11 13:55:22 -0500

    wires: towire/fromwire for wally_tx
    We're eventually moving away from 'bitcoin_tx

--- wire/onion_printgen.c
+++ wire/onion_printgen.c
@@ -653,4 +653,4 @@ void printonion_wire_tlv_message(const char *tlv_name, const u8 *msg) {
 		printwire_tlvs(tlv_name, &msg, &plen, print_tlvs_tlv_payload, ARRAY_SIZE(print_tlvs_tlv_payload));
 	}
 }
-// SHA256STAMP:43b0761145cf51e4de37f46c966c199ba305d02ca43e03350151e8435b9b1153
+// SHA256STAMP:8d6a5352571ee00c7afa5ceceaf833208723ee570779db62c30fd3db0a7b5c90
