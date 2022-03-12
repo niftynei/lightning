dc3a3731166d673f61b7726b42e724eb284edb42
niftynei <niftynei@gmail.com>
2022-03-12 17:17:06 -0600

    bkpr: exclude non-wallet events in the balance snapshot
    Anchor outputs are ignored by the clightning wallet, but we keep track
    of them in the bookkeeper. This causes problems when we do the balance
    checks on restart w/ the balance_snapshot -- it results in us printing
    out a journal_entry to 'get rid of' the anchors that the clightning node
    doesnt know about.
    
    Instead, we mark some outputs as 'ignored' and exclude these from our
    account balance sums when we're comparing to the clightning snapshot.

--- plugins/bkpr/db.c
+++ plugins/bkpr/db.c
@@ -92,6 +92,7 @@ static struct migration db_migrations[] = {
 	NULL},
 	{SQL("ALTER TABLE chain_events ADD origin VARCHAR(64);"), NULL},
 	{SQL("ALTER TABLE accounts ADD closed_count INTEGER DEFAULT 0;"), NULL},
+	{SQL("ALTER TABLE chain_events ADD ignored INTEGER;"), NULL},
 };
 
 static bool db_migrate(struct plugin *p, struct db *db)
--- tests/test_connection.py
+++ tests/test_connection.py
@@ -656,8 +656,12 @@ def test_reconnect_no_update(node_factory, executor, bitcoind):
     disconnects = ["-WIRE_FUNDING_LOCKED", "-WIRE_SHUTDOWN"]
     # Allow bad gossip because it might receive WIRE_CHANNEL_UPDATE before
     # announcement of the disconnection
-    l1 = node_factory.get_node(may_reconnect=True, allow_bad_gossip=True)
-    l2 = node_factory.get_node(disconnect=disconnects, may_reconnect=True)
+    l1 = node_factory.get_node(may_reconnect=True, allow_bad_gossip=True,
+                               # No bookkeeper, gets hammered by memleak
+                               options={'disable-plugin': 'bookkeeper'})
+    l2 = node_factory.get_node(disconnect=disconnects, may_reconnect=True,
+                               # No bookkeeper, gets hammered by memleak
+                               options={'disable-plugin': 'bookkeeper'})
 
     # For channeld reconnection
     l1.rpc.connect(l2.info["id"], "localhost", l2.port)
