8beb74d6480a3bd298512b393d868b4ff34219fe
niftynei <niftynei@gmail.com>
2022-03-30 11:35:49 -0500

    coin_mvt/bkpr: add "stealable" tag to stealable outputs
    If we expect further events for an onchain output (because we can steal
    it away from the 'external'/rightful owner), we mark them.
    
    This prevents us from marking a channel as 'onchain-resolved' before
    all events that we're interested in have actually hit the chain.
    
    Case that this matters:
    Peer publishes a (cheating) unilateral close and a timeout htlc (which
    we can steal).
    We then steal the timeout htlc.
    
    W/o the stealable flag, we'd have marked the channel as resolved when
    the peer published the timeout htlc, which is incorrect as we're still
    waiting for the resolution of that timeout htlc (b/c we *can* steal it).

--- plugins/bkpr/db.c
+++ plugins/bkpr/db.c
@@ -93,6 +93,7 @@ static struct migration db_migrations[] = {
 	{SQL("ALTER TABLE chain_events ADD origin VARCHAR(64);"), NULL},
 	{SQL("ALTER TABLE accounts ADD closed_count INTEGER DEFAULT 0;"), NULL},
 	{SQL("ALTER TABLE chain_events ADD ignored INTEGER;"), NULL},
+	{SQL("ALTER TABLE chain_events ADD stealable INTEGER;"), NULL},
 };
 
 static bool db_migrate(struct plugin *p, struct db *db)
